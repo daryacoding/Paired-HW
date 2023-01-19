087a9f726f54d11188eb539807d7b5620e5fb7b0
Josue <cuellarjosue3@gmail.com>
2023-01-18 16:01:15 -0800

    First challenge

---
+++ first.js
@@ -0,0 +1,8 @@
+// Challenge 1
+// Create a function addTwothat accepts one input and adds 2 to it.
+ function addTwo(num) {
+
+// return the number plus 2
+    return num + 2
+}
+console.log(addTwo(5))
