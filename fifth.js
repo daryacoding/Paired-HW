b908cec6a34abebd69c766089bc44b39d938b6ee
Josue <cuellarjosue3@gmail.com>
2023-01-21 16:24:18 -0800

    ignore modules

---
+++ fifth.js
@@ -0,0 +1,22 @@
+/*
+Challenge 5
+Construct a json api with the ExpressJS Framework. You will not use a database. You will mock a database of blogposts in a seperate JS file with the following starter data ***Do in Python if you can with flask***
+const mockBlog = [
+    {
+        id: 1,
+        title: 'First Blog Post',
+        description: 'My first blog post!'
+    },
+    {
+        id: 2,
+        title: 'Second Blog Post',
+        description: 'My Second blog post!'
+    },
+    {
+        id: 3,
+        title: 'Third Blog Post',
+        description: 'My Third blog post!'
+    }
+];
+module.exports = mockBlog
+*/
